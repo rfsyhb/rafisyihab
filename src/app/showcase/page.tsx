06a169d870ff79d49572660c08b721df13713997
rfsyhb <rafisyihab3@gmail.com>
2026-03-05 19:50:36 +0700

    fix: correct height unit from vh to dvh

--- src/app/showcase/page.tsx
+++ src/app/showcase/page.tsx
@@ -34,7 +34,7 @@ export default async function ShowcasePage({
           </Link>
         ))}
       </aside>
-      <main className="w-full flex border border-text rounded-2xl p-2 shadow-lg h-[88vh] md:h-[70vh] lg:h-[50vh] overflow-hidden md:flex-1">
+      <main className="w-full flex border border-text rounded-2xl p-2 shadow-lg h-[88dvh] md:h-[70dvh] lg:h-[50dvh] overflow-hidden md:flex-1">
         {selectedId ? (
           <div className="flex flex-col md:flex-row h-full w-full gap-2 overflow-hidden">
             <div className="order-2 md:order-1 flex flex-col gap-1 flex-1 overflow-hidden min-h-0">
